a89f7ffbe6f41dfcf12e5ef73128ea93438aca3c
Carlos-Cardenas-Bravo <cardenasbravo@gmail.com>
2024-09-28 00:10:52 -0300

    Versión Final

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: films
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  synopsis   :text
+#  director   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Film < ApplicationRecord
     validates :name, presence: true
     validates :synopsis, presence: true
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: series
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  synopsis   :text
+#  director   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Serie < ApplicationRecord
     validates :name, presence: true
     validates :synopsis, presence: true
