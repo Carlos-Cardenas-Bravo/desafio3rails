807be4379b06907b58772129e932070d733a3eb1
Carlos-Cardenas-Bravo <cardenasbravo@gmail.com>
2024-09-29 15:18:13 -0300

    modif de seeds.rb y render-build.sh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,25 @@
 require 'faker'
 
-directores = [
-  "Steven Spielberg",
-  "Martin Scorsese",
-  "Quentin Tarantino",
-  "Christopher Nolan",
-  "Alfred Hitchcock",
-  "James Cameron",
-  "Ridley Scott",
-  "Woody Allen",
-  "David Fincher",
-  "Coen Brothers"
-]
-
-series = [
-  "Breaking Bad",
-  "Game of Thrones",
-  "Stranger Things",
-  "The Crown",
-  "The Mandalorian",
-  "Friends",
-  "The Office",
-  "Black Mirror",
-  "The Big Bang Theory",
-  "Sherlock"
-]
-
-# Generar 20 películas
-20.times do
+10.times do
   Movie.create(
     name: Faker::Movie.title,
-    synopsis: Faker::Lorem.paragraph,
-    director: directores.sample 
+    synopsis: Faker::Lorem.sentence(word_count: 15),
+    director: Faker::Name.name
   )
 end
 
-# Generar 20 series
-20.times do
+10.times do
   Film.create(
-    name: series.sample, 
-    synopsis: Faker::Lorem.paragraph,
-    director: directores.sample 
+    name: Faker::TvShows::Friends.character, 
+    synopsis: Faker::Lorem.sentence(word_count: 15),
+    director: Faker::Name.name
   )
 end
 
-# Generar 20 documentales
-20.times do
+10.times do
   Documentary.create(
-    name: Faker::Educator.course_name,
-    synopsis: Faker::Lorem.paragraph,
-    director: directores.sample 
+    name: Faker::Educator.course_name, 
+    synopsis: Faker::Lorem.sentence(word_count: 15),
+    director: Faker::Name.name
   )
 end
